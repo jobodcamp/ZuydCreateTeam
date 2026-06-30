5c5d78179d54c1f6c0238778bb8a919078e3fa25
jobodcamp <jobopdencamp24@gmail.com>
2026-06-30 09:40:00 +0200

    Add files via upload

---
+++ Zuyd-Create-Team-main/FesitvalAppv2.1/Models/Vrijwilliger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FestivalAppv2.Models
+{
+    public class Vrijwilliger : Persoon
+    {
+        //Properties
+        public int VrijwilligerId { get; private set; }
+        public string Email { get; private set; }
+        public Vrijwilliger(int vrijwilligerId, string naam, string email) : base(naam)
+        {
+            VrijwilligerId = vrijwilligerId;
+            Email = email;
+        }
+
+        //Geeft het vrijwilligerId terug
+        public int GetVrijwilligerId()
+        {
+            return VrijwilligerId;
+        }
+
+        //Return email
+        public string GetEmail()
+        {
+            return Email;
+        }
+
+        //Toont de informatie van de vrijwilliger
+        public override string ToonInfo()
+        {
+            return VrijwilligerId + " - " + Naam + " (" + Email + ")";
+        }
+    }
+}
